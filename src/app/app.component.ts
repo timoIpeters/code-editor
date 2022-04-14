4f378c12716872d576beccc618149be9a769f465
Timo Peters <timo.peters1702@gmail.com>
2022-04-14 13:23:10 +0200

    Add codemirror, Add codemirror starting example using html, css and javascript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { materialize } from 'rxjs';
+
+const defaults = {
+  'text/javascript': `const string = 'Hello World';
+console.log(string);\n`,
+  'text/html': `<p>Hello World!</p>\n`,
+  'text/css': `body {
+    color: red;
+  }\n`
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'code-editor';
+  readOnly = false;
+  mode: keyof typeof defaults = 'text/javascript';
+  options = {
+    lineNumbers: true,
+    theme: 'material',
+    gutter: true,
+    mode: this.mode,
+  };
+  defaults = defaults;
+
+  changeMode(): void {
+    this.options = {
+      ...this.options,
+      mode: this.mode,
+    };
+  }
+
+  handleChange($event: Event): void {
+    console.log('ngModelChange', $event);
+  }
+
+  clear(): void {
+    this.defaults[this.mode] = '';
+  }
 }
